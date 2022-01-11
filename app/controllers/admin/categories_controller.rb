a429436e012be37ef4c8414b302d10d7aee3b9af
Matthew Parisien <matthewparisien4@gmail.com>
2022-01-11 15:49:22 +0000

    Add user view

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,9 +1,11 @@
+
 class Admin::CategoriesController < ApplicationController
-  
-  http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_USERNAME'], password: ENV['HTTP_BASIC_AUTH_PASSWORD']
+
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+  password: ENV["HTTP_BASIC_PASSWORD"]
 
   def index
-    @categories = Category.all
+    @categories = Category.order(id: :desc).all
   end
 
   def new
@@ -14,7 +16,7 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to [:admin, :categories], notice: "Category Created!"
+      redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
@@ -23,6 +25,8 @@ class Admin::CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:category).permit(:name)
+    params.require(:category).permit(
+      :name
+    )
   end
 end
