368c5db05469939e1c90c7def5404d08ea954fe9
Matthew Parisien <matthewparisien4@gmail.com>
2022-01-12 21:21:42 +0000

    Add more routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,11 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+  
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
