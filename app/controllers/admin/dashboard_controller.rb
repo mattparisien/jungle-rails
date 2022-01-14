53de6eff0568bbc06f7d2c7beefe2547e6dde885
Matt Parisien <matthewparisien4@gmail.com>
2022-01-14 12:54:32 -0500

    Add styling to navbar

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+  password: ENV["HTTP_BASIC_PASSWORD"]
+
   def show
     @products_total = Product.count
     @categories_total = Category.count
