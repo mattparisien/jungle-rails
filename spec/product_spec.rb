efc9beee8a2f856e5205436a9807a32c20681bf0
Matt Parisien <matthewparisien4@gmail.com>
2022-01-24 15:59:48 -0500

    Add first category test

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,4 +1,18 @@
-describe 'Validations' do
-  # validation tests/examples here
-end
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+
+    #validates :category, presence: true
+
+    it "is a valid product with the correct details" do
+      category = Category.new(name: 'Electronics')
+      product = Product.new(
+        name:  'Modern Skateboards',
+        description: "Banjo post-ironic intelligentsia. Irony ramps austin. Slow-carb drinking whatever sartorial seitan occupy. Seitan street mixtape.",
+        category: category,
+        quantity: 40,
+        price: 164.49
+      )
+      expect(product).to be_valid
+    end
+  end
 end
