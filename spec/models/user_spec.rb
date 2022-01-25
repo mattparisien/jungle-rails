f29312e3a4f87ae74fa19ffa9a0bc219ed82c76a
Matt Parisien <matthewparisien4@gmail.com>
2022-01-25 10:05:29 -0500

    Add user model in app dir

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,28 @@ RSpec.describe User, type: :model do
 
     it "is a valid user" do
       user = User.new(
-        name: 'user_name',
+        name: 'name',
         email: 'hello@testing.com',
         password: 'lovebird',
         password_confirmation: 'lovebird'
       )
       expect(user).to be_valid
+    end
+
+    it "is tests for password min length" do
+         user = User.new
+         user.name = "Matt"
+         user.email = 'hello@testing.com'
+         user.password = 'lovebird'
+         user.password_confirmation = 'lovebird'
+         expect(user).to be_invalid
+         
+         user = User.new
+         user.name = "name"
+         user.email = 'test@test.com'
+         user.password = 'bird'
+         user.password_confirmation = 'bird'
+         expect(user).to be_valid
     end
 
   end
