8a23c926ccb89e63ff87351a389d8c19d37219cd
Matt Parisien <matthewparisien4@gmail.com>
2022-01-25 09:14:39 -0500

    Implement price fail test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -44,6 +44,19 @@ RSpec.describe Product, type: :model do
       expect(product).to be_valid
     end
 
+    #validates :category, presence: true
+    it "should fail if the product has no category" do
+      category = Category.new(name: 'Electronics')
+      product = Product.new(
+        name:  'Modern Skateboards',
+        description: "Banjo post-ironic intelligentsia. Irony ramps austin. Slow-carb drinking whatever sartorial seitan occupy. Seitan street mixtape.",
+        category: nil,
+        quantity: 40,
+        price: 164.49
+      )
+      expect(product).to be_valid
+    end
+
 
   end
 end
