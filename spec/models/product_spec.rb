06b25b31f01e00a9d8862e8a073bc96b4cfaf95c
Matt Parisien <matthewparisien4@gmail.com>
2022-01-25 09:11:46 -0500

    Implement quantity

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -29,5 +29,21 @@ RSpec.describe Product, type: :model do
       )
       expect(product).to be_valid
     end
+
+  
+    #validates :quantity, presence: true
+    it "should fail if the product has no quantity" do
+      category = Category.new(name: 'Electronics')
+      product = Product.new(
+        name:  'Modern Skateboards',
+        description: "Banjo post-ironic intelligentsia. Irony ramps austin. Slow-carb drinking whatever sartorial seitan occupy. Seitan street mixtape.",
+        category: category,
+        quantity: nil,
+        price: 164.49
+      )
+      expect(product).to be_valid
+    end
+
+
   end
 end
