88b14af4141230ac6fdf39c439380ec1d3926fba
Matt Parisien <matthewparisien4@gmail.com>
2022-01-25 09:04:37 -0500

    Add product name validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,11 +5,23 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
 
     #validates :category, presence: true
-
     it "is a valid product with the correct details" do
       category = Category.new(name: 'Electronics')
       product = Product.new(
         name:  'Modern Skateboards',
+        description: "Banjo post-ironic intelligentsia. Irony ramps austin. Slow-carb drinking whatever sartorial seitan occupy. Seitan street mixtape.",
+        category: category,
+        quantity: 40,
+        price: 164.49
+      )
+      expect(product).to be_valid
+    end
+
+    #validates :name, presence: true
+    it "should fail if the product has no name" do
+      category = Category.new(name: 'Electronics')
+      product = Product.new(
+        name:  nil,
         description: "Banjo post-ironic intelligentsia. Irony ramps austin. Slow-carb drinking whatever sartorial seitan occupy. Seitan street mixtape.",
         category: category,
         quantity: 40,
