556849efcd89dcc232737589ba32592530e57410
Matthew Parisien <matthewparisien4@gmail.com>
2022-01-25 18:56:49 +0000

    Add images.rb

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -10,6 +9,7 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
       )
@@ -24,6 +24,7 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     save_screenshot
 
     # VERIFY
-    expect(page).to have_css 'article.product'
+    expect(page).to have_css 'article.product', count: 10
+    puts page.html
   end
 end
