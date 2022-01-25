12508f2051c1598699d992cb8dba527f0270dd15
Matthew Parisien <matthewparisien4@gmail.com>
2022-01-25 19:08:10 +0000

    Implement testing for add to cart functionality

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "Add to cart works", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+  end
+
+  scenario "can add products to cart" do
+    # ACT
+    visit root_path
+    expect(page).to have_content('My Cart (0)')
+
+    # VERIFY
+    click_button('Add')
+    expect(page).to have_content('My Cart (1)')
+
+    # DEBUG
+    save_screenshot
+    puts page.html
+  end
+end
