383ed4ef920237e7b77f891db26a564ecc6957eb
Matthew Parisien <matthewparisien4@gmail.com>
2022-01-25 14:23:54 +0000

    Reimplement testing strategy from start

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,62 +1,18 @@
 require 'rails_helper'
 
-
 RSpec.describe Product, type: :model do
   describe 'Validations' do
 
-    #validates :category, presence: true
-    it "is a valid product with the correct details" do
-      category = Category.new(name: 'Electronics')
-      product = Product.new(
-        name:  'Modern Skateboards',
-        description: "Banjo post-ironic intelligentsia. Irony ramps austin. Slow-carb drinking whatever sartorial seitan occupy. Seitan street mixtape.",
-        category: category,
-        quantity: 40,
-        price: 164.49
-      )
-      expect(product).to be_valid
-    end
+    it "tests validatation" do 
+      @product = Product.new
+      @category = Category.new
+      @category.name = "Electronics"
+      @product.name = "Red Bookshelf"
+      @product.price_cents = 99555
+      @product.quantity = 10
+      @product.category = @category
 
-    #validates :name, presence: true
-    it "should fail if the product has no name" do
-      category = Category.new(name: 'Electronics')
-      product = Product.new(
-        name:  nil,
-        description: "Banjo post-ironic intelligentsia. Irony ramps austin. Slow-carb drinking whatever sartorial seitan occupy. Seitan street mixtape.",
-        category: category,
-        quantity: 40,
-        price: 164.49
-      )
-      expect(product).to be_valid
-    end
-
-  
-    #validates :quantity, presence: true
-    it "should fail if the product has no quantity" do
-      category = Category.new(name: 'Electronics')
-      product = Product.new(
-        name:  'Modern Skateboards',
-        description: "Banjo post-ironic intelligentsia. Irony ramps austin. Slow-carb drinking whatever sartorial seitan occupy. Seitan street mixtape.",
-        category: category,
-        quantity: nil,
-        price: 164.49
-      )
-      expect(product).to be_valid
-    end
-
-    #validates :category, presence: true
-    it "should fail if the product has no category" do
-      category = Category.new(name: 'Electronics')
-      product = Product.new(
-        name:  'Modern Skateboards',
-        description: "Banjo post-ironic intelligentsia. Irony ramps austin. Slow-carb drinking whatever sartorial seitan occupy. Seitan street mixtape.",
-        category: nil,
-        quantity: 40,
-        price: 164.49
-      )
-      expect(product).to be_valid
-    end
-
-
+      expect(@product.valid?).to be true
+    end    
   end
 end
